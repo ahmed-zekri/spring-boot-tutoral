f7e01b2f05b63944791d78efba8aa5b2c6d4cafe
ahmed103 <ahmed.zekri.a@gmail.com>
2022-01-30 23:00:12 +0100

    Exposing social media

--- src/main/java/com/example/demo/User/UserController.java
+++ src/main/java/com/example/demo/User/UserController.java
@@ -1,42 +1,60 @@
 package com.example.demo.User;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.spring.web.json.Json;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
-@RequestMapping("api/")
+@RequestMapping("/api")
 public class UserController {
-    private final UserService userService;
 
-    @Autowired
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
 
-    @GetMapping("/users")
-    public List<UserTable> getUsers() {
-        return userService.getUsers();
-    }
+    @GetMapping("/response_time")
+    public Json getUsers() throws IOException {
+        OkHttpClient client = new OkHttpClient().newBuilder()  .connectTimeout(30, TimeUnit.SECONDS)
+                .writeTimeout(30, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .build();
+        Request request = new Request.Builder()
 
-    @PostMapping("/add_user")
-    public void createUser(@RequestBody UserTable user) {
-        userService.addUser(user);
-
-    }
-
-    @DeleteMapping("/delete_user/{id}")
-    public void deleteUser(@PathVariable Long id) {
-        if (userService.findUserById(id).isPresent())
-            userService.deleteUser(id);
-        else throw new IllegalStateException("User not found");
+                .url("https://social-media-stats-and-data.herokuapp.com/response_time")
+//                .method("GET", null)
+//                .addHeader("Content-Type", "application/json")
+//                .addHeader("cache-control", "no-cache")
+//                .addHeader("Authorization", "Bearer EAAMdU4MhaoABAA4bJlofLZCfQwlhTZAOY1SMZBbQi91pDZCZB9ETeAV0znZBw3r3Gr7FaBMSiZCZCVYfrsPSKRoHwf71oRGSDwqyDwectnM4GGvXYjYqrhAOpkPcZB8mHLNyupz7yiQIDWz7LqaZBgRKnWArVsgOQJgf5DRyudwrJoLpGq0ZB4qLYs5")
+                .build();
+        Response response = client.newCall(request).execute();
+        if (response.body() != null) {
+            return new Json(response.body().string());
+        }
+        return new Json("");
 
     }
 
-    @PutMapping("/update_user/{id}")
-    public void updateUser(@PathVariable Long id, @RequestParam(required = false) String name) {
-        userService.updateUser(id, name);
-
-    }
+//    @PostMapping("/add_user")
+//    public void createUser(@RequestBody UserTable user) {
+//        userService.addUser(user);
+//
+//    }
+//
+//    @DeleteMapping("/delete_user/{id}")
+//    public void deleteUser(@PathVariable Long id) {
+//        if (userService.findUserById(id).isPresent())
+//            userService.deleteUser(id);
+//        else throw new IllegalStateException("User not found");
+//
+//    }
+//
+//    @PutMapping("/update_user/{id}")
+//    public void updateUser(@PathVariable Long id, @RequestParam(required = false) String name) {
+//        userService.updateUser(id, name);
+//
+//    }
 }
